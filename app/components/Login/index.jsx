15a4348955fbbd4ea71454b103bc6885ed66b008
宋天恩 <songtianen@sina.com>
2017-07-11 23:53:36 +0800

    react:login

---
+++ app/components/Login/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+// 导入性能优化
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import './style.less'
+class Login extends React.Component{
+    constructor(props,context){
+      super(props,context);
+      this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+      this.state={
+        phone:''
+      }
+    }
+    render(){
+      console.log(this.props.loginHandle)
+      return(
+        <div id = "login-container">
+          <div className="input-container phone-container">
+            <i className='icon-tablet'></i>
+            <input
+              type="text"
+              placeholder="输入手机号"
+              onChange={this.changeHandle.bind(this)}
+              value={this.state.phone}/>
+            </div>
+          <div className="input-container password-container">
+            <i className="icon-key"></i>
+            <button>发送验证码</button>
+            <input type="text" placeholder="输入验证码"/>
+          </div>
+            <button className="btn-login" onClick={this.clickHandle.bind(this)}>登陆</button>
+        </div>
+      )
+    }
+    changeHandle(e){
+      this.setState({
+        phone:e.target.value
+      })
+    }
+    clickHandle(e){
+      const username = this.state.phone
+      const loginHandle = this.props.loginHandle
+      loginHandle(username)
+
+    }
+}
+
+export default Login
---
+++ app/containers/Login/index.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+// 导入性能优化
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { hashHistory } from 'react-router'
+import * as userInfoActionsFromOtherFile from '../../redux/actions/userinfo'
+// 引入header
+import Header from '../../components/Header'
+
+import LoginComponent from '../../components/Login'
+
+
+
+
+class Login extends React.Component{
+    constructor(props,context){
+      super(props,context);
+      this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+      this.state={
+        // 正在检查
+        checking:true
+      }
+    }
+    render(){
+      return(
+        <div>
+          <Header title="登陆"/>
+          {
+
+            this.state.checking
+            ?<div>{/*等待。。。*/}</div>
+            :<LoginComponent loginHandle ={ this.loginHandle.bind(this) }>这里是将要展示登陆的组件</LoginComponent>
+          }
+        </div>
+      )
+    }
+    componentDidMount() {
+      // 执行检查的方法
+      this.doCheck()
+    };
+    doCheck(){
+      // 从reudx 中获取 userinfo
+      const userinfo = this.props.userinfo;
+      if(userinfo.username){
+        //已经登陆
+        this.goUserPage()
+      }else{
+        //尚未登陆
+        this.setState({
+          checking:false
+        });
+      };
+    };
+    // 区用户中心页面
+    goUserPage(){
+      // 没有登陆就调到个人中心。
+      hashHistory.push('/User');
+    }
+
+    // 登陆之后的业务处理 用智能组件来做
+    loginHandle(username){
+      //保存用户名
+      const actions = this.props.userInfoActions
+      let userinfo = this.props.userinfo;
+      userinfo.username = username
+      actions.update(userinfo)
+      //跳转连接
+      const params = this.props.params
+      const router = params.router
+      if(router) {
+        //跳转指定的页面
+        hashHistory.push(router)
+      }else {
+        //跳转到默认页面 就是 用户中心页面
+        this.goUserPage();
+      }
+    }
+}
+
+// -----redux
+function mapStateToProps(state){
+  return {
+    userinfo:state.userinfo
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    userInfoActions:bindActionCreators(userInfoActionsFromOtherFile,dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login)
